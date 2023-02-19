3d7152c353706d6fc8a1314086492ffe17bc5467
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-19 21:18:36 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+    galleryEl.insertAdjacentHTML("beforeend",cardsMarkup);
 
-console.log(galleryItems);
+            function createGalleryCardsMarkup(gallery) {
+            return gallery.map(({preview,original,description}) => {
+            return `<li>
+                <a class="gallery__item" href="${original}">
+                <img src="${preview}" alt="${description}" class="gallery__image">
+                </a>
+                </li>`;
+                })
+                .join("");
+                }
+
+                const lightbox = new SimpleLightbox(".gallery__item", {
+                    captionDelay:250,
+                    captionsData:"alt",
+            });
+
+    console.log(galleryItems);
+
