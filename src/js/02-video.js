4e89b80411c6f04463b0e32796b7cabb55f7de9f
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-19 23:19:20 +0200

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,36 +1,36 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+    import Player from "@vimeo/player";
+    import throttle from "lodash.throttle";
 
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+        const iframe = document.querySelector('iframe');
+        const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+        player.on('play', function() {
+            console.log('played the video!');
+        });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+        player.getVideoTitle().then(function(title) {
+            console.log('title:', title);
+        });
 
-const currentTime = function (data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
-    let time = Number(localStorage.getItem("videoplayer-current-time"));
-    console.log(time);
-}
-
-player.on("timeupdate", throttle(currentTime, 1000));
-
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
-    
-}).catch(function(error) {
-    switch (error.name) {
-case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
+    const currentTime = function (data) {
+        localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+        let time = Number(localStorage.getItem("videoplayer-current-time"));
+        console.log(time);
     }
-});
+
+    player.on("timeupdate", throttle(currentTime, 1000));
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+        
+    }).catch(function(error) {
+        switch (error.name) {
+    case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,51 @@
 import throttle from "lodash.throttle";
 
-const refs = {
-    form: document.querySelector(".feedback-form"),
-    textarea: document.querySelector(".feedback-form textarea"),
-    email: document.querySelector(".feedback-form input"),
-};
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.input.addEventListener("input", onInput);
+refs.textarea.addEventListener("input", throttle(onTextarea, 500));
+
 const formData = {};
 const STORAGE_KEY = "feedback-form-state";
-const parsedData = localStorage.getItem(STORAGE_KEY, JSON.parse(formData));
-// const savedData = localStorage.getItem(STORAGE_KEY, JSON.stringify(formData));
 
-populateMessageOutput();
+populateForm();
 
-refs.form.addEventListener("input", e => {
+function onInput(e) {
     formData[e.target.name] = e.target.value;
-})
-refs.form.addEventListener("submit", onFormSubmit);
-refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
-
-function onTextareaInput(evt) {
-    // const message = evt.target.value;
-    return localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-};
-
-function onFormSubmit(evt){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function onTextarea(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(e) {
     evt.preventDefault();
+    const savedData = localStorage.getItem(STORAGE_KEY, JSON.stringify(formData));
+    const parsedData = localStorage.getItem(STORAGE_KEY, JSON.parse(savedData));
+    console.log(parsedData);
+
 
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
     console.log(formData);
-};
+}
+
+
+// refs.form.addEventListener("input", e => {
+// })
+// refs.form.addEventListener("submit", onFormSubmit);
+// refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
+
+// function onTextareaInput(evt) {
+//     // const message = evt.target.value;
+//     return localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// };
+
+// function onFormSubmit(evt){
+//     evt.preventDefault();
+
+//     evt.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+//     console.log(formData);
+// };
 
