f68a45d615d3c4b1720ccff40ddc783354e8748b
AntoninaKobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-19 19:18:58 +0000

    Deploying to gh-pages from @ AntoninaKobzar/goit-js-hw-08@3d7152c353706d6fc8a1314086492ffe17bc5467 🚀

---
+++ 01-gallery.55d90151.js
@@ -0,0 +1,2 @@
+!function(){galleryEl.insertAdjacentHTML("beforeend",cardsMarkup);new SimpleLightbox(".gallery__item",{captionDelay:250,captionsData:"alt"});console.log(galleryItems)}();
+//# sourceMappingURL=01-gallery.55d90151.js.map
---
+++ 01-gallery.7fd69aa5.js
@@ -0,0 +1,2 @@
+galleryEl.insertAdjacentHTML("beforeend",cardsMarkup);new SimpleLightbox(".gallery__item",{captionDelay:250,captionsData:"alt"});console.log(galleryItems);
+//# sourceMappingURL=01-gallery.7fd69aa5.js.map
