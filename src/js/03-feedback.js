a284f781147144676e7c9fe7e292ac09b707dd8b
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-20 18:05:34 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,9 +2,9 @@ import throttle from "lodash.throttle";
 const STORAGE_KEY = "feedback-form-state";
 const formData = {};
 const refs = {
-    form: document.querySelector("feedback-form"),
-    textarea: document.querySelector("feedback-form textarea"),
-    input: document.querySelector("feedback-form input"),
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+    input: document.querySelector(".feedback-form input"),
     
 };
 
@@ -15,8 +15,9 @@ refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
 
 refs.form.addEventListener("input", e => {
     formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     console.log(formData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(JSON.stringify(formData));
 });
 
 
@@ -27,25 +28,23 @@ function onTextareaInput(evt) {
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    console.log("send form");
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    
 }
-// function populateTextarea() {
-//     const savedMessage = localStorage.getItem(STORAGE_KEY,JSON.parse(formData));
-//     if (savedMessage) {
-//         refs.textarea.value = savedMessage;
-//     }
-// }
+
 function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY,JSON.parse(formData));
-    if (load(STORAGE_KEY)) {
-        const outputForm = load(STORAGE_KEY);
-        const formKeys = formKeys.map(element => document.querySelector(`[name="${element}"]`).value = outputForm[element]);
-        // refs.textarea.value = savedMessage;
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    console.log(savedMessage);
+    if (savedMessage) {
+        const savedMessageObject = JSON.parse(savedMessage);
+        console.log(savedMessageObject.email);
+        const email = document.querySelector("input");
+        email.value = savedMessageObject.email;
+        const textarea = document.querySelector("textarea");
+        textarea.value = savedMessageObject.message;
     }
 }
-   
 
 
 
